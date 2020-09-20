940a69c3137ed41774fa0d26867cb656cb60cc1e
Charles Duyk <charles.duyk@gmail.com>
2020-09-20 08:51:34 -1000

    Initial Commit

---
+++ METAR/METARApp.swift
@@ -0,0 +1,17 @@
+//
+//  METARApp.swift
+//  METAR
+//
+//  Created by Charles Duyk on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct METARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
